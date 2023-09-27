73d6fc218170a712d4b46d7c6897daeaa1d5520d
joseph-wee <starlightkor@gmail.com>
2023-09-27 11:42:36 +0900

    design: 디자인 레이아웃 변경  Please enter the commit message for your changes. Lines starting

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { getMenu } from '@/lib/api'
-import Link from 'next/link'
 import './globals.css'
+import Link from 'next/link'
 
 export const metadata = {
   title: 'Create Next App',
@@ -8,27 +8,25 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-
   const menu = getMenu()
-
-
-
 
   return (
     <html lang="en">
-      <body className="pl-[200px]">
-        <div className='fixed w-[200px] h-screen left-0 bg-slate-500' >
+      <body className="pl-[318px] pt-[18px]">
+        <div className='pt-3 fixed w-[300px] h-screen left-0 top-0 bg-slate-400' >
           {
             menu.map((el) => {
               return (
                 <div>
-                  {Object.keys(el)}
-                  <div className='pl-[15px]'>
-                    {el[Object.keys(el)].map((menu) => {
+                  <div className='h-9 flex items-center pl-5'>
+                    {Object.keys(el)}
+                  </div>
+                  <div className='pl-12 text-xm'>
+                    {el[Object.keys(el)].map((nav) => {
                       return (
-                        <Link href={`${menu}`}>
-                          <div>
-                            {menu}
+                        <Link href={`post/${nav.replace(/ /g, "_")}`}>
+                          <div className='h-9 flex items-center'>
+                            {nav}
                           </div>
                         </Link>
                       )
@@ -39,7 +37,9 @@ export default function RootLayout({ children }) {
             })
           }
         </div>
-        <div>{children}</div></body>
+        <div className='mx-auto max-w-3xl'>
+          {children}
+        </div></body>
     </html>
   )
 }
